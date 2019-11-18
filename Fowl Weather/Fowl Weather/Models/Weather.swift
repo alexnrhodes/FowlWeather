2fd17be40f7f3ab0aa05575e004ededf32029312
Jake <jlconnerly@gmail.com>
2019-11-18 14:52:18 -0600

    Weather model built

--- Fowl Weather/Fowl Weather/Models/Weather.swift
+++ Fowl Weather/Fowl Weather/Models/Weather.swift
@@ -8,4 +8,88 @@
 
 import Foundation
 
-//testing local Develop push to proper remote Develop branch
+struct WeatherDay: Codable {
+    let cityName: String
+    let weather: [String]
+    let temp: Double
+    let tempMin: Double
+    let tempMax: Double
+    let windSpeed: Double
+    let windDirection: Int
+    let sunrise: Date
+    let sunset: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case cityName = "name"
+        case weather
+        case temp = "main"
+        case tempMin
+        case tempMax
+        case windSpeed = "wind"
+        case windDirection
+        case sunrise = "sys"
+        case sunset
+        
+        enum WeatherDescriptionKeys: String, CodingKey {
+            case weatherDescription = "description"
+        }
+        
+        enum TempKeys: String, CodingKey {
+            case temp
+            case tempMin = "temp_min"
+            case tempMax = "temp_max"
+        }
+        
+        enum WindKeys: String, CodingKey {
+            case windSpeed = "speed"
+            case windDirection = "deg"
+        }
+        
+        enum SunKeys: String, CodingKey {
+            case sunrise
+            case sunset
+        }
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        // Top Level
+        let cityName = try container.decode(String.self, forKey: .cityName)
+        self.cityName = cityName
+        
+        // Weather Descriptions
+        var weatherDiscriptions: [String] = []
+        var weatherContainerArray = try container.nestedUnkeyedContainer(forKey: .weather)
+        while !weatherContainerArray.isAtEnd {
+            let weatherContainer = try weatherContainerArray.nestedContainer(keyedBy: CodingKeys.WeatherDescriptionKeys.self)
+            let weatherDescription = try weatherContainer.decode(String.self, forKey: .weatherDescription)
+            weatherDiscriptions.append(weatherDescription)
+        }
+        self.weather = weatherDiscriptions
+        
+        // Temps
+        let mainContainter = try container.nestedContainer(keyedBy: CodingKeys.TempKeys.self, forKey: .temp)
+        let temp = try mainContainter.decode(Double.self, forKey: .temp)
+        let tempMin = try mainContainter.decode(Double.self, forKey: .tempMin)
+        let tempMax = try mainContainter.decode(Double.self, forKey: .tempMax)
+        self.temp = temp
+        self.tempMin = tempMin
+        self.tempMax = tempMax
+        
+        // Winds
+        let windContainer = try container.nestedContainer(keyedBy: CodingKeys.WindKeys.self, forKey: .windSpeed)
+        let windSpeed = try windContainer.decode(Double.self, forKey: .windSpeed)
+        let windDirection = try windContainer.decode(Int.self, forKey: .windDirection)
+        self.windSpeed = windSpeed
+        self.windDirection = windDirection
+        
+        //Sunrise and Sunset
+        let sunContainer = try container.nestedContainer(keyedBy: CodingKeys.SunKeys.self, forKey: .sunrise)
+        let sunrise = try sunContainer.decode(Date.self, forKey: .sunrise)
+        let sunset = try sunContainer.decode(Date.self, forKey: .sunset)
+        self.sunrise = sunrise
+        self.sunset = sunset
+    }
+}

34794fc7a054eb840805ae00817cbb137a86e63a
Jake <jlconnerly@gmail.com>
2019-11-18 11:42:35 -0600

    initial commit

---
+++ Fowl Weather/Fowl Weather/Model Controllers/WeatherController.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherController.swift
+//  Fowl Weather
+//
+//  Created by Jake Connerly on 11/18/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import Foundation
---
+++ Fowl Weather/Fowl Weather/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fowl Weather
+//
+//  Created by Jake Connerly on 11/18/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

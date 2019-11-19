c95a566fda2350d602a0cc84ce2194ff6582e50c
Alexandra Rhodes <anrhodes5738@gmail.com>
2019-11-19 13:35:35 -0600

    Changed DadJoke to UILabel

--- Fowl Weather/Fowl Weather/View Controllers/ViewController.swift
+++ Fowl Weather/Fowl Weather/View Controllers/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var dayLabel: UILabel!
-    @IBOutlet weak var dadJokeTextView: UITextView!
+    @IBOutlet weak var dadJokeLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
     @IBOutlet weak var tempLabel: UILabel!
@@ -21,10 +21,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var windDirectionLabel: UILabel!
     @IBOutlet weak var sunriseLabel: UILabel!
     @IBOutlet weak var sunsetLabel: UILabel!
+    @IBOutlet weak var carouselCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        carouselCollectionView.dataSource = self
+        carouselCollectionView.delegate = self
     }
 
     @IBAction func searchButtonTapped(_ sender: UIButton) {
@@ -33,3 +35,20 @@ class ViewController: UIViewController {
     
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "WeatherCell", for: indexPath) as? WeatherCollectionViewCell else {return UICollectionViewCell()}
+        
+        cell.layer.cornerRadius = 10
+        cell.backgroundColor = UIColor(red: 255/255.0, green: 204/255.0, blue: 0/255.0, alpha: 0.35)
+        
+        return cell
+        
+    }
+   
+}
